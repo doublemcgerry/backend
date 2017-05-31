cb70dd3ad6e13b4879224befe280c126609fc8f3
achelius <rompamail2@gmail.com>
2017-05-31 17:23:14 +0200

    added actor announce javadoc

--- src/main/java/rz/thesis/server/serialization/action/management/ActorAnnounceAction.java
+++ src/main/java/rz/thesis/server/serialization/action/management/ActorAnnounceAction.java
@@ -1,5 +1,12 @@
 package rz.thesis.server.serialization.action.management;
 
+/**
+ * this class defines the base structure for an actorAnnounceAction, which
+ * contains basically the device independent attributes
+ * 
+ * @author achelius
+ *
+ */
 public abstract class ActorAnnounceAction extends ManagementAction {
 
 	private static final long serialVersionUID = 6722305545748319370L;
