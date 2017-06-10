e703746ae2a56b11501cf9373f945617f146496f
LorenzoZoia <lollozoia@hotmail.it>
2017-06-10 12:25:30 +0200

    Creted package for experience management

---
+++ src/main/java/rz/thesis/server/serialization/action/lobby/experience/ExperienceAction.java
@@ -0,0 +1,9 @@
+package rz.thesis.server.serialization.action.lobby.experience;
+
+import rz.thesis.server.serialization.action.Action;
+
+public abstract class ExperienceAction extends Action {
+
+	private static final long serialVersionUID = 4835175166936107364L;
+
+}
