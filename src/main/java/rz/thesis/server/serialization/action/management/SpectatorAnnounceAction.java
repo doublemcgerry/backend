ea32e6c637456d720e8ea4863befae5ae7de8afa
achelius <rompamail2@gmail.com>
2017-06-14 11:35:19 +0200

    Revert "Auto stash before merge of "SecondSeason" and "origin/SecondSeason""
    This reverts commit 809fc47c18fe742d3b6351e4135c6ee2254abc0a.

--- src/main/java/rz/thesis/server/serialization/action/management/SpectatorAnnounceAction.java
+++ src/main/java/rz/thesis/server/serialization/action/management/SpectatorAnnounceAction.java
@@ -1,2 +1,29 @@
+package rz.thesis.server.serialization.action.management;
 
+import rz.thesis.server.lobby.LobbiesManagerInterface;
+import rz.thesis.server.lobby.Subscriber;
+import rz.thesis.server.lobby.actors.concrete.SpectatorConcrete;
+<<<<<<< Updated upstream
+import rz.thesis.server.serialization.action.auth.AuthCodeAction;
+=======
+>>>>>>> Stashed changes
+
+public class SpectatorAnnounceAction extends ActorAnnounceAction {
+
+	private static final long serialVersionUID = -3018483622075602666L;
+
+	public SpectatorAnnounceAction() {
+	}
+
+	@Override
+	public void execute(LobbiesManagerInterface router, Subscriber wrapper) {
+		SpectatorConcrete actor = new SpectatorConcrete(wrapper);
+<<<<<<< Updated upstream
+		String token = router.addLobbyActorToWaitingRoom(actor);
+		wrapper.sendAction(wrapper, new AuthCodeAction(token));
+=======
 		router.addActorToLobby(wrapper.getServerSession().getUsername(), actor);
+>>>>>>> Stashed changes
+	}
+
+}
