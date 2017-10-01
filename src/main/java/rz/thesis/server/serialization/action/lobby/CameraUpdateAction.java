2998174d7d50a873a291a11fe930e20cae5eab8d
Achelius <rompamail2@gmail.com>
2017-10-02 00:42:30 +0200

    added some attributes on cameraupdateaction for future use

--- src/main/java/rz/thesis/server/serialization/action/lobby/CameraUpdateAction.java
+++ src/main/java/rz/thesis/server/serialization/action/lobby/CameraUpdateAction.java
@@ -1,5 +1,7 @@
 package rz.thesis.server.serialization.action.lobby;
 
+import java.util.List;
+
 import rz.thesis.modules.experience.ExperiencesModule;
 import rz.thesis.server.lobby.LobbiesManagerInterface;
 import rz.thesis.server.lobby.ServerLobby;
@@ -8,15 +10,37 @@ import rz.thesis.server.utility.Quaternion;
 
 public class CameraUpdateAction extends LobbyAction {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6296841742192781098L;
 	private Quaternion cameraRotation;
 	private Quaternion deltaRotation;
 	private int deltaTime;
+	private List<Quaternion> lastValues;
+	private List<Integer> lastDeltas;
 
 	@Override
 	public void execute(LobbiesManagerInterface lobbyManager, ExperiencesModule experiencesModule, ServerLobby lobby,
 			VirtualActor actor) {
 		// this.deltaRotation = Vector3.DivideByFloat(deltaRotation, this.deltaTime);
 		lobby.broadcastAction(this);
+	}
+
+	public Quaternion getCameraRotation() {
+		return cameraRotation;
+	}
+
+	public Quaternion getDeltaRotation() {
+		return deltaRotation;
+	}
+
+	public int getDeltaTime() {
+		return deltaTime;
+	}
+
+	public List<Quaternion> getLastValues() {
+		return lastValues;
 	}
 
 }
